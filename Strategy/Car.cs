a648b13e3ad5b1c4e3ccbbd29be8c562fa6770fb
Malena-AV <m.alena07082003@gmail.com>
2023-04-10 21:07:15 +0300

    Добавьте файлы проекта.

---
+++ Strategy/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Car
+    {
+        // паттерн который позволяет использовать разную реализацию в одном поведении
+        private IMovement movement;
+        public Car(IMovement movement)
+        {
+            this.movement = movement; 
+        }
+
+        public void Move()
+        {
+            movement.Move();
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Electrecity electrecity = new Electrecity();
+            Gas gas = new Gas();
+            Petrol petrol = new Petrol();
+
+            Car car1 = new Car(electrecity);
+            Car car2 = new Car(gas);
+            Car car3 = new Car(petrol);
+
+            car1.Move();
+            car2.Move();
+            car3.Move();
+
+            Console.ReadKey();
+        }
+    }
+}
